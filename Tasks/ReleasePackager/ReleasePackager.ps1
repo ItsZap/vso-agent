fea97bb6c77b2e9650d73a0498d311447ea14fe5
Riwut Libinuko <cakriwut@gmail.com>
2016-02-14 19:20:55 +0800

    Search nuget tools

--- Tasks/ReleasePackager/ReleasePackager.ps1
+++ Tasks/ReleasePackager/ReleasePackager.ps1
@@ -3,23 +3,67 @@ param (
     [string]$versionByBuild
 )
 
-Write-Verbose 'Entering script $MyInvocation.MyCommand.Name'
-Write-Verbose "Parameter Values"
+Write-Host "Entering script $($MyInvocation.MyCommand.Name)"
+Write-Host "Parameter Values"
 foreach($key in $PSBoundParameters.Keys)
 {
 	
-	Write-Verbose ("{0} = {1}" -f $key,$PSBoundParameters[$key])
+	Write-Host ("    {0} = {1}" -f $key,$PSBoundParameters[$key])
 }
-Write-Verbose "Importing modules"
+Write-Host "Importing modules"
 Import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
 Import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 
-# Prepare working tools
-$nuGetPath = Get-ToolPath -Name 'NuGet.exe';
-if(-not $nugetPath)
+# Static references
+$nugetOriginalUrl = "https://nuget.org/nuget.exe"
+
+# Helpers functions
+function DownloadFile($source,$destination)
 {
-	Write-Verbose ("Unable to locate {0}" -f 'nuget.exe')
+    $client = New-Object System.Net.WebClient
+    $client.DownloadFile($source,$destination)
 }
 
+function FindNuget($searchLoc)
+{
+    $findNuget = gci -Path $searchLoc -Filter "nuget.exe" -Recurse | sort LastWriteTime | select -Last 1
+    if($findNuget -eq $null)
+    {
+       Write-Host "Not found!" -ForegroundColor Red
+       return $null       
+    } 
 
-Write-Verbose 'Leaving script $MyInvocation.MyCommand.Name'
+    return $findNuget.FullName
+ }
+
+# Step 1. Prepare the tools Nuget.exe
+
+Write-Host "Check if I can find default Nuget.exe" -NoNewline
+$nuGetPath = Get-ToolPath -Name 'NuGet.exe'
+if(-not $nugetPath)
+{
+   Write-Host "Not found!" -ForegroundColor Red
+   Write-Host "Check if I can find in working directory $PSScriptRoot " -NoNewline
+   $nugetPath = FindNuget -searchLoc $PSScriptRoot
+
+   if(-not $nuGetPath)
+   {
+        Write-Host "Try to download from internet.." -NoNewline
+        DownloadFile -source $nugetOriginalUrl -destination $PSScriptRoot\nuget.exe
+        $nugetPath = FindNuget -searchLoc $PSScriptRoot
+        if(-not $nuGetPath)
+        {
+           throw("Can not find Nuget.exe")
+        }
+   }
+} 
+   
+Write-Host "Found! Using Nuget binary path: $nugetPath" -ForegroundColor Green
+
+# Step 2. Combine *.nuspec file. Follow name convention Main.nuspec , [*].Main.nuspec example File1.Main.nuspec, File2.Main.nuspec
+
+# Step 3. Execute Nuget packager
+
+# Step 4. Upload Relase package to galler
+
+Write-Host 'Leaving script $MyInvocation.MyCommand.Name'
