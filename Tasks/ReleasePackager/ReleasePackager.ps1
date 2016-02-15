0e9d6658a4fa16ef418edb3713735e8f3eeb98e7
Riwut Libinuko <cakriwut@users.noreply.github.com>
2016-02-16 02:44:01 +0800

    Add nuspec combinator

--- Tasks/ReleasePackager/ReleasePackager.ps1
+++ Tasks/ReleasePackager/ReleasePackager.ps1
@@ -36,6 +36,38 @@ function FindNuget($searchLoc)
     return $findNuget.FullName
  }
 
+# Step 0. Check if environment setting is available
+
+if(-not ($env:BUILD_SOURCESDIRECTORY -and $env:BUILD_BUILDNUMBER))
+{
+   Write-Error "Running script from outside VSO Agent is currently not supported"
+   exit 1
+}
+
+if(!$mainNuspec)
+{
+   throw ("Main nuspec release file must be set")
+}
+
+$fnameToPackage = [System.IO.Path]::GetFileName($mainNuspec)
+$fpathToPackage = gci -Path $env:BUILD_SOURCESDIRECTORY -Filter $fnameToPackage -Recurse
+
+write-host " --MainFile: $($fpathToPackage.FullName)"
+[xml]$mainXml = gc $fpathToPackage.FullName
+
+gci -Path $env:BUILD_SOURCESDIRECTORY -Filter *.$fnameToPackage -Recurse |% {
+
+   write-host "   ^__ PartialFile: $($_.FullName)"
+   [xml]$partialXml = gc $_.FullName
+   foreach($node in $partialXml.package.files.ChildNodes)
+   {
+      $none = $mainXml.SelectNodes("//*[local-name()='files']").AppendChild($mainXml.ImportNode($node,$true))
+   }
+}
+
+# Save to mainlocation
+$mainXml.Save($fpathToPackage.FullName)
+
 # Step 1. Prepare the tools Nuget.exe
 
 Write-Host "Check if I can find default Nuget.exe" -NoNewline
@@ -45,19 +77,18 @@ if(-not $nugetPath)
    Write-Host "Not found!" -ForegroundColor Red
    Write-Host "Check if I can find in working directory $PSScriptRoot " -NoNewline
    $nugetPath = FindNuget -searchLoc $PSScriptRoot
-}
 
-if(-not $nuGetPath)
-{
-   Write-Host "Try to download from internet.." -NoNewline
-   DownloadFile -source $nugetOriginalUrl -destination $PSScriptRoot\nuget.exe
-   $nugetPath = FindNuget -searchLoc $PSScriptRoot
-}
-   
-if(-not $nuGetPath)
-{
-    throw("Can not find Nuget.exe")
-}
+   if(-not $nuGetPath)
+   {
+        Write-Host "Try to download from internet.." -NoNewline
+        DownloadFile -source $nugetOriginalUrl -destination $PSScriptRoot\nuget.exe
+        $nugetPath = FindNuget -searchLoc $PSScriptRoot
+        if(-not $nuGetPath)
+        {
+           throw("Can not find Nuget.exe")
+        }
+   }
+} 
    
 Write-Host "Found! Using Nuget binary path: $nugetPath" -ForegroundColor Green
 
@@ -67,4 +98,4 @@ Write-Host "Found! Using Nuget binary path: $nugetPath" -ForegroundColor Green
 
 # Step 4. Upload Relase package to galler
 
-Write-Host 'Leaving script $MyInvocation.MyCommand.Name'
+Write-Host "Leaving script $($MyInvocation.MyCommand.Name)"
