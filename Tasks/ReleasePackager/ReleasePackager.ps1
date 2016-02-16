68d004934cbbc6ec49967e3c6ed1e680153ed5a6
Riwut Libinuko <cakriwut@gmail.com>
2016-02-16 08:14:45 +0800

    Complete packaging. Add logo

--- Tasks/ReleasePackager/ReleasePackager.ps1
+++ Tasks/ReleasePackager/ReleasePackager.ps1
@@ -1,8 +1,9 @@
 param (
     [string]$mainNuspec,
-    [string]$versionByBuild
+    [string]$versionByBuild,
+    [string]$vsoOutputDir
 )
-
+gc $PSScriptRoot\logo.txt
 Write-Host "Entering script $($MyInvocation.MyCommand.Name)"
 Write-Host "Parameter Values"
 foreach($key in $PSBoundParameters.Keys)
@@ -15,7 +16,8 @@ Import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
 Import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 
 # Static references
-$nugetOriginalUrl = "https://nuget.org/nuget.exe"
+#$nugetOriginalUrl = "https://nuget.org/nuget.exe"
+$nugetOriginalUrl = "http://dist.nuget.org/win-x86-commandline/latest/nuget.exe"  #version 3.x
 
 # Helpers functions
 function DownloadFile($source,$destination)
@@ -35,6 +37,41 @@ function FindNuget($searchLoc)
 
     return $findNuget.FullName
  }
+
+ function PrepareSrcPath($node,$basePath)
+ {
+     $nodeSrc = $node.src
+     if($nodeSrc.StartsWith("*"))
+     {
+          $nodeSrc = "\" + $nodeSrc
+     }
+     if($nodeSrc.StartsWith("\"))
+     {
+         $nodeSrc = $basePath + $nodeSrc
+     }
+     $node.src = $nodeSrc
+     return $node
+ }
+
+ function GetTargetPath($fileref)
+ {
+    return (gci (Split-Path -Path $fileref -Parent) $env:BuildConfiguration -Recurse -Directory |? { 
+                                $_.FullName.Contains("\bin\") } | select -First 1)
+ }
+
+# Step 0. Check if environment setting is available
+
+if(-not ($env:BUILD_SOURCESDIRECTORY -and $env:BUILD_BUILDNUMBER))
+{
+   Write-Error "Running script from outside VSO Agent is currently not supported"
+   exit 1
+}
+
+if(!$mainNuspec)
+{
+   throw ("Main nuspec release file must be set")
+}
+
 
 # Step 1. Prepare the tools Nuget.exe
 
@@ -62,8 +99,42 @@ Write-Host "Found! Using Nuget binary path: $nugetPath" -ForegroundColor Green
 
 # Step 2. Combine *.nuspec file. Follow name convention Main.nuspec , [*].Main.nuspec example File1.Main.nuspec, File2.Main.nuspec
 
+$fnameToPackage = [System.IO.Path]::GetFileName($mainNuspec)
+$fpathToPackage = gci -Path $env:BUILD_SOURCESDIRECTORY -Filter $fnameToPackage -Recurse
+
+write-host " --MainFile: $($fpathToPackage.FullName)"
+[xml]$mainXml = gc $fpathToPackage.FullName
+$outputdir = $(GetTargetPath -fileref $fpathToPackage.FullName).FullName
+
+foreach($node in $mainXml.package.files.ChildNodes)
+{    
+    $node = PrepareSrcPath -node $node -basePath $outputdir
+}
+
+# Merging
+gci -Path $env:BUILD_SOURCESDIRECTORY -Filter *.$fnameToPackage -Recurse |% {
+
+   #Base path 
+   $srcBasePath = GetTargetPath -fileref $_.FullName
+   write-host "   ^__ PartialFile: $($_.FullName)"
+   [xml]$partialXml = gc $_.FullName
+   foreach($node in $partialXml.package.files.ChildNodes)
+   {
+      $node = PrepareSrcPath -node $node -basePath $srcBasePath.FullName     
+      $none = $mainXml.SelectNodes("//*[local-name()='files']").AppendChild($mainXml.ImportNode($node,$true))
+   }
+}
+
+# Save to mainlocation
+$mainXml.Save($fpathToPackage.FullName)
+
 # Step 3. Execute Nuget packager
 
-# Step 4. Upload Relase package to galler
+$argsPack = "pack `"$($fpathToPackage.FullName)`" -OutputDirectory `"$outputdir`" ";
+$slnFolder = "C:\"
+Invoke-Tool -Path $nugetPath -Arguments "$argsPack" -WorkingFolder $slnFolder
 
-Write-Host 'Leaving script $MyInvocation.MyCommand.Name'
+# Step 4. Upload Relase package to gallery
+Write-Verbose "**************************"
+
+Write-Host "Leaving script $($MyInvocation.MyCommand.Name)"
