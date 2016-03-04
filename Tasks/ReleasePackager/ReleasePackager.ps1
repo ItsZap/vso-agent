df8e5994b2c8a5074a6e63d825790c46b801d802
Riwut Libinuko <cakriwut@gmail.com>
2016-03-04 21:55:32 +0800

    Extract common function in external function

--- Tasks/ReleasePackager/ReleasePackager.ps1
+++ Tasks/ReleasePackager/ReleasePackager.ps1
@@ -3,16 +3,8 @@ param (
     [string]$versionByBuild,
     [string]$vsoOutputDir
 )
-gc $PSScriptRoot\logo.txt
-Write-Host "Entering script $($MyInvocation.MyCommand.Name)"
-Write-Host "Parameter Values"
-foreach($key in $PSBoundParameters.Keys)
-{	
-	Write-Host ("    {0} = {1}" -f $key,$PSBoundParameters[$key])
-}
-Write-Host "Importing modules"
-Import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
-Import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+.\Initialize.ps1
 
 # Static references
 #$nugetOriginalUrl = "https://nuget.org/nuget.exe"
